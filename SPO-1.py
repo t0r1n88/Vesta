020d0051f81efb22c8a84de29b8db559ce0941a2
Oleg Budaev <t0r1n88@mail.ru>
2023-08-12 11:08:42 +0800

    начало рефакторинга, удалил лишние файлы

--- SPO-1.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import openpyxl
-
-df= pd.read_excel('data/Общий список БРИТ 26.11.2021.xlsx')
-print(df.head())
--- create_folder.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-
-df = pd.read_excel('data/Список регионов РФ.xlsx')
-lst_reg = df['Наименование'].tolist()
-path_to_end = 'data/folder'
-for reg in lst_reg:
-    if not os.path.isdir(f'{path_to_end}/{reg}'):
-        os.mkdir(f'{path_to_end}/{reg}')
